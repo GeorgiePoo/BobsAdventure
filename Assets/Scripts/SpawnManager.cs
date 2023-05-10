875977a474b9f7581e5ebcbb36bb26d7ad425145
GeorgiePoo <gepurugganan2@s.sfusd.edu>
2023-05-09 22:39:06 -0700

    Spawn Manager For Enemies

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject[] enemyPrefabs;
     private Vector3 spawnPos = new Vector3(23, -1, -1);
+    public Vector2 spawnTimeRange;
+    public int numberOfEnemies;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
+        StartCoroutine(SpawnEnemies());
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private IEnumerator SpawnEnemies()
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+
+            GameObject randomEnemyPrefab = enemyPrefabs[Random.Range(0, enemyPrefabs.Length)];
+            Instantiate(randomEnemyPrefab, spawnPos, randomEnemyPrefab.transform.rotation);
+
+            //Random Time Spawns
+            float waitTime = Random.Range(spawnTimeRange.x, spawnTimeRange.y);
+            yield return new WaitForSeconds(waitTime);
+        }
         
     }
 }
